96474234542b3f8bbae1603b81ae38abed7ab31a
aaguasca <arnauaguasca@gmail.com>
2022-10-25 15:31:54 +0200

    add temp. res. vs sig.

--- trets/visualization.py
+++ trets/visualization.py
@@ -3,10 +3,12 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import astropy.units as u
 
 __all__=[
     "temporal_resolution_hist",
-    "significance_distribution"
+    "significance_distribution",
+    "temporal_resolution_vs_sig"
 ]
 
 def temporal_resolution_hist(ax,flux_points,temporal_units):
@@ -33,9 +35,9 @@ def temporal_resolution_hist(ax,flux_points,temporal_units):
     mask_ul=flux_points["is_ul"]==True
 
     #flux points
-    time_array=(flux_TRETS["time_max"][~mask_ul.reshape(-1)]-flux_TRETS["time_min"][~mask_ul.reshape(-1)])*u.day
+    time_array=(flux_points["time_max"][~mask_ul.reshape(-1)]-flux_points["time_min"][~mask_ul.reshape(-1)])*u.day
     #UL
-    time_array_ul=(flux_TRETS["time_max"][mask_ul.reshape(-1)]-flux_TRETS["time_min"][mask_ul.reshape(-1)])*u.day
+    time_array_ul=(flux_points["time_max"][mask_ul.reshape(-1)]-flux_points["time_min"][mask_ul.reshape(-1)])*u.day
 
     time_array=time_array.to_value(temporal_units)
     n=ax.hist(time_array,bins=50,label="Flux point")
@@ -48,7 +50,7 @@ def temporal_resolution_hist(ax,flux_points,temporal_units):
     return ax
 
 
-def significance_distribution(ax,flux_points_table):
+def significance_distribution(ax,flux_points):
     """
     Plot the significance of the fluxes of the flux container 
     flux_points as a function of time.
@@ -57,7 +59,7 @@ def significance_distribution(ax,flux_points_table):
     ----------
     ax:
         Axis
-    flux_points_table: astropy.table.table.Table
+    flux_points: astropy.table.table.Table
         Container Table for flux points as gammapy. Required column names
         time_min, time_max, sig_detection, is_ul.
 
@@ -76,10 +78,55 @@ def significance_distribution(ax,flux_points_table):
     plt.plot(x[mask_ul.reshape(-1)],flux_points["sig_detection"][mask_ul.reshape(-1)],"o",label="UL")
 
     xmin,xmax=ax.get_xlim()
-    ax.hlines(flux_points.meta["sig-THD"],xmin,xmax,label="Threshold")
-    ax.set_ylabel("Significance")
+    ax.hlines(flux_points.meta["SIG-THD"],xmin,xmax,label="Threshold",color="k")
+    ax.set_ylabel(r"Significance [$\sigma$]")
     ax.set_xlabel("Time [MJD]")
-    plt.legend(loc="best")
+    plt.legend(loc="lower left")
 
     return ax
 
+
+
+def temporal_resolution_vs_sig(ax,list_tab,temporal_units):
+    """
+    Plot the median value of the temporal resolution histogram
+    as a function of the detection significance threshold.
+    By default, edges do the boxes delimiters the range between 
+    the 25th and 75th interpercentile, and the error bars delimiters
+    the 5th to 95th interpercentile range.
+    """
+    
+    Parameters
+    ----------
+    ax:
+        Axis    
+    list_tab: list
+        List with the table of flux points for different source detection
+        significance thresholds.
+    temporal_units: astropy.units
+        Units to express the y-axis
+
+    Returns
+    -------
+    ax:
+        Axis
+
+    list_sig_detection=[]
+    list_t_bins=[]
+    for tab in list_tab:
+        list_t_bins.append(
+            ((tab["time_max"]-tab["time_min"]).value*u.d).to_value(temporal_units)
+        )
+        list_sig_detection.append(tab.meta["SIG-THD"])
+    
+    ax.boxplot(
+        list_t_bins,
+        positions=list_sig_detection,
+        whis=[5,95],
+        sym=""
+    )
+
+    ax.set_xlabel(r"Detection statistical significance [$\sigma$]")
+    ax.set_ylabel(r"Median time-bin distribution [{}]".format(temporal_units))   
+
+    return ax    
